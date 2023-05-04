e33255a522031bca2fa2d8b071e135a992041d54
pochodyla <pochodyla@gmail.com>
2023-05-04 16:02:17 +0200

    vec2whole

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -3,48 +3,53 @@
 
 
 template<typename T>
-class Vec2
+class Vec2_
 {
 public:
-	Vec2(T x_in, T y_in)
+	Vec2_() = default;
+	Vec2_(T x_in, T y_in)
 		:
 		x(x_in),
 		y(y_in)
+	{}
+	Vec2_(const Vec2_<float>& src)
+		:
+		x((T)src.x),
+		y((T)src.y)
+	{}
+	Vec2_ operator+(const Vec2_& rhs) const
 	{
-	}
-	Vec2 operator+(const Vec2& rhs) const
-	{
-		return Vec2(x + rhs.x, y + rhs.y);
+		return Vec2_(x + rhs.x, y + rhs.y);
 	}
 
-	Vec2& operator+=(const Vec2& rhs)
+	Vec2_& operator+=(const Vec2_& rhs)
 	{
 		return *this = *this + rhs;
 	}
 
-	Vec2 operator*(T rhs) const
+	Vec2_ operator*(T rhs) const
 	{
-		return Vec2(x * rhs, y * rhs);
+		return Vec2_(x * rhs, y * rhs);
 	}
 
-	Vec2& operator*=(T rhs)
+	Vec2_& operator*=(T rhs)
 	{
 		return *this = *this * rhs;
 	}
 
-	Vec2 operator-(const Vec2& rhs) const
+	Vec2_ operator-(const Vec2_& rhs) const
 	{
-		return Vec2(x - rhs.x, y - rhs.y);
+		return Vec2_(x - rhs.x, y - rhs.y);
 	}
 
-	Vec2& operator-=(const Vec2& rhs)
+	Vec2_& operator-=(const Vec2_& rhs)
 	{
 		return *this = *this - rhs;
 	}
 
 	T GetLength() const
 	{
-		return std::sqrt(GetLengthSq());
+		return (T)std::sqrt(GetLengthSq());
 	}
 
 	T GetLengthSq() const
@@ -52,17 +57,17 @@ public:
 		return x * x + y * y;
 	}
 
-	Vec2& Normalize()
+	Vec2_& Normalize()
 	{
 		return *this = GetNormalized();
 	}
 
-	Vec2 GetNormalized() const
+	Vec2_ GetNormalized() const
 	{
 		const T len = GetLength();
-		if (len != 0.0f)
+		if (len != T(0))
 		{
-			return *this * (1.0f / len);
+			return *this * ((T)1 / len);
 		}
 		return *this;
 	}
@@ -72,7 +77,8 @@ public:
 };
 
 
-
+typedef Vec2_<float> Vec2;
+typedef Vec2_<int> Vei2;
 
 
 
